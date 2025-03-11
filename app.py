7ffc7b07eea89259885be4de16c9821a9e786ef5
Hosea <karanja.mungai23@gmail.com>
2025-03-11 15:11:23 +0300

    adds user routes PUT,GET,DELETE

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from models import db
+from flask import Flask, jsonify, request
+from models import db,User
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 from flask_cors import CORS
@@ -14,3 +14,32 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
+@app.route('/', methods=['GET'])
+def index():
+    return (jsonify({'message': 'Welcome to Moringa IMS API'}), 200)
+
+@app.route('/users',methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.to_dict() for user in users]), 200
+
+@app.route('/users/<int:user_id>', methods=['GET', 'PUT', 'DELETE'])
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        return jsonify({'message': 'User not found'}), 404
+    if request.method == 'GET':
+        return jsonify(user.to_dict()), 200
+    if request.method == 'PUT':
+        data = request.get_json()
+        user.username = data['username']
+        user.email = data['email']
+        user.contact = data['contact']
+        user.password = data['password']
+        db.session.commit()
+        return jsonify(user.to_dict()), 200
+    if request.method == 'DELETE':
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': 'User deleted'}), 200

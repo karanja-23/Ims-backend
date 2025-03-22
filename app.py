35be34bde1664f01bb9f8a12b23dfac4dc5faf45
Hosea <karanja.mungai23@gmail.com>
2025-03-22 16:32:11 +0300

    modifies history table so dates are automatically entered with new entries

--- app.py
+++ app.py
@@ -391,9 +391,9 @@ def add_asset_history(asset_id):
         return jsonify({'message': 'Asset not found'}), 404
     data = request.get_json()
     status = data.get('status')
-    date = data.get('date')
     assigned_to = data.get('assigned_to')
-    history = FixedAssetHistory(fixed_asset_id=asset_id, status=status, date=date,assigned_to=assigned_to)
+    space_id = data.get('space_id')
+    history = FixedAssetHistory(fixed_asset_id=asset_id, status=status,assigned_to=assigned_to)
     db.session.add(history)
     db.session.commit()
     return jsonify({'message': 'Asset history added'}), 201
